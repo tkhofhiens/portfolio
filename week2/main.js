0123464621a833244d7bae21b7fc33857ee668a4
Tiffany <tiffquinlan@gmail.com>
2020-09-25 15:48:40 -0700

    Week 2 update

--- week2/main.js
+++ week2/main.js
@@ -1,5 +1,34 @@
 const message = 'Hello World!'
 alert(message)
+
 const uniqueID = Symbol('this is a unique ID');
 console.log(uniqueID);
-console.log(sum(range(1, 10)));
+const name = { value: 'Alexa' }; // an object
+name.value = 'Siri'; // change the value
+
+// Set the questions
+const quiz = new Map([
+    ["What is Superman's real name?","Clark Kent"],
+    ["What is Wonderwoman's real name?","Dianna Prince"],
+    ["What is Batman's real name?","Bruce Wayne"]
+  ]);
+
+ // initialize score
+let score = 0;
+
+for(const [question,answer] of quiz.entries()){
+
+  // get answer from user
+  const response = prompt(question);
+  // check if answer is correct
+  if(response === answer){
+    alert('Correct!');
+    // increase score by 1
+    score++;
+  } else {
+    alert(`Wrong! The correct answer was ${answer}`);
+  }
+}
+
+// At the end of the game, report the player's score
+alert(`Game Over, you scored ${score} point${score > 1 ? 's' : ''}`);
