d197d68c3f9f21b0d60afc3359e9fe5f00cb9deb
Tiffany <tiffquinlan@gmail.com>
2020-09-25 16:11:14 -0700

    week2 update

--- week2/main.js
+++ week2/main.js
@@ -1,34 +1,41 @@
 // const message = 'Hello World!'
 // alert(message)
 
-const uniqueID = Symbol('this is a unique ID');
-console.log(uniqueID);
-const name = { value: 'Alexa' }; // an object
-name.value = 'Siri'; // change the value
+// const uniqueID = Symbol('this is a unique ID');
+// console.log(uniqueID);
+// const name = { value: 'Alexa' }; // an object
+// name.value = 'Siri'; // change the value
 
-// Set the questions
-const quiz = new Map([
-    ["What is Superman's real name?","Clark Kent"],
-    ["What is Wonderwoman's real name?","Dianna Prince"],
-    ["What is Batman's real name?","Bruce Wayne"]
-  ]);
+const btn = document.getElementById('button');
+const rainbow = ['red','orange','yellow','green','blue','rebeccapurple','violet'];
+function change() {
+    document.body.style.background = rainbow[Math.floor(7 * Math.random())];
 
- // initialize score
-let score = 0;
+    // Set the questions
+    const quiz = new Map([
+        ["What is Superman's real name?", "Clark Kent"],
+        ["What is Wonderwoman's real name?", "Dianna Prince"],
+        ["What is Batman's real name?", "Bruce Wayne"]
+    ]);
 
-for(const [question,answer] of quiz.entries()){
+    // initialize score
+    let score = 0;
 
-  // get answer from user
-  const response = prompt(question);
-  // check if answer is correct
-  if(response === answer){
-    alert('Correct!');
-    // increase score by 1
-    score++;
-  } else {
-    alert(`Wrong! The correct answer was ${answer}`);
-  }
+    for (const [question, answer] of quiz.entries()) {
+
+        // get answer from user
+        const response = prompt(question);
+        // check if answer is correct
+        if (response === answer) {
+            alert('Correct!');
+            // increase score by 1
+            score++;
+        } else {
+            alert(`Wrong! The correct answer was ${answer}`);
+        }
+    }
+
+    // At the end of the game, report the player's score
+    alert(`Game Over, you scored ${score} point${score > 1 ? 's' : ''}`);
 }
-
-// At the end of the game, report the player's score
-alert(`Game Over, you scored ${score} point${score > 1 ? 's' : ''}`);
+btn.addEventListener('click', change);
