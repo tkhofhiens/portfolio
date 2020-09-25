3fc15b03b271e770a7758a74a16c89a001613606
Tiffany <tiffquinlan@gmail.com>
2020-09-25 16:24:42 -0700

    Update main.js

--- week2/main.js
+++ week2/main.js
@@ -2,19 +2,19 @@
 // alert(message)
 
 const uniqueID = Symbol(`player's unidue ID is:`);
-console.log(uniqueID,math.random);
+console.log(uniqueID,Math.random());
 // const name = { value: 'Alexa' }; // an object
 // name.value = 'Siri'; // change the value
 
 const backcolor = document.getElementById('background');
+const rainbow = ['red','orange','yellow','green','blue','rebeccapurple','violet'];
 function change() {
     document.body.style.background = rainbow[Math.floor(7 * Math.random())];
 }
 backcolor.addEventListener('click', change);
 
+function quiz() {
 const btn = document.getElementById('game');
-const rainbow = ['red','orange','yellow','green','blue','rebeccapurple','violet'];
-
     // Set the questions
     const quiz = new Map([
         ["What is Superman's real name?", "Clark Kent"],
@@ -42,4 +42,4 @@ const rainbow = ['red','orange','yellow','green','blue','rebeccapurple','violet'
     // At the end of the game, report the player's score
     alert(`Game Over, you scored ${score} point${score > 1 ? 's' : ''}`);
 }
-btn.addEventListener('click', change);
+btn.addEventListener('click', quiz);
