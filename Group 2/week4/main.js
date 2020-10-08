198901cab7c0325899a1c9bba27bd853ddd291ab
tiffquinlan <tiffquinlan@gmail.com>
2020-10-08 15:45:29 -0700

    notes and team project

---
+++ Group 2/week4/main.js
@@ -0,0 +1,29 @@
+const board = document.querySelector('.board');
+const resetButton = document.getElementById('reset');
+const player1 = 'X';
+const player2 = 'O';
+let player = player1;
+
+function addPiece(e) {
+    console.log(e.target);
+    e.target.innerHTML = player;
+    if (player === player1) player = player2;
+    else player = player1;
+}
+
+
+
+function resetBoard() {
+    console.dir(board);
+    for (let i = 0; i < board.rows.length; i++) {
+        let row = board.rows[i];
+        for (let j = 0; j < row.cells.length; j++) {
+            row.cells[j].innerHTML = '';
+        }
+    }
+}
+
+board.addEventListener('click', addPiece);
+resetButton.addEventListener('click', resetBoard);
+
+
