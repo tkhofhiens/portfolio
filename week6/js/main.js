ee33d3d2edcffd8d5cd1fb30164449603277c980
tkhofhiens <tkhofhiens@gmail.com>
2020-10-24 15:31:16 -0700

    toggle works

--- week6/js/main.js
+++ week6/js/main.js
@@ -81,7 +81,6 @@ function toggleComplete(e){
     console.log(e.currentTarget);
     console.log('completed Btn is ',btn.completeBtn);
     console.log('completed is ',btn.completed);
-    console.log('todo.completed is ',todo.completed);
     if (btn.getAttribute('completed') === 'false'){
         btn.setAttribute('completed', true);
         btn.innerHTML = '&#10004';  
