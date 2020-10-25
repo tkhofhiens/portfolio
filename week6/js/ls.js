ee45112de3a5bd88ae6b17566177ce7fe608126f
tkhofhiens <tkhofhiens@gmail.com>
2020-10-24 19:43:05 -0700

    update

--- week6/js/ls.js
+++ week6/js/ls.js
@@ -28,8 +28,9 @@ function updateTodo(id, status){
     // find the todo item in list
     console.log('updateTodo Function: id ', id, 'status', status);
     toDoList.forEach(todo =>{
+        checkId = todo.id.toString();
         console.log('todo id', todo.id, 'comp', todo.completed);
-        if(todo.id === id){
+        if(checkId === id){
             todo.completed = status;
             console.log('ls completed ', todo.completed, 'was updated');
         }
