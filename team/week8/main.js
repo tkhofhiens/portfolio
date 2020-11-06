b912da07dbd6b6303bb986fd118877d8fda251e4
Tiffany <tiffquinlan@gmail.com>
2020-11-06 15:56:53 -0700

    team code stretch goal

--- team/week8/main.js
+++ team/week8/main.js
@@ -1,24 +1,3 @@
-// function learnSW(url) {
-
-
-//     fetch(url)
-//     .then((response) => {
-//         if(response.ok) {
-//             return response;
-//         }
-//         throw Error(response.statusText);
-//     })
-//     .then(response => response.json()) 
-//     .then(data => console.log(Object.defineProperties(data)))
-//     .catch( error => console.log(response.statusText) )
-//     }
-
-//     function buildStuff(response) {
-
-//         text = JSON.parse(response)
-//         document.getElementsByClassName('onward').innerHTML = `<button onClick="learnSW(` + text.next + `)">Onward</button>`;  
-//     }
-// }
 
 async function learnSW(url) {
     try {
@@ -37,24 +16,42 @@ async function learnSW(url) {
 const nextBtn = document.querySelector('#onward');
 const backBtn = document.querySelector('#back');
 
-function renderPeople(data){
+function renderPeople(data) {
     //clear curent list to show the next or previous 10 people
     document.querySelector('#peopleList').innerHTML = "";
-    
+
     const starwarsContainer = document.querySelector('#peopleList');
     data.results.forEach(people => {
         const listItem = document.createElement('li');
-        listItem.innerText = people.name;
+        // listItem.innerText = people.name;
+        listItem.innerHTML =
+            `
+        <button class="view-btn" onclick="showDetails('${people.name}','${people.height}','${people.mass}')">${people.name}</button>
+        `
+        listItem.id = people.name;
         starwarsContainer.appendChild(listItem);
     })
 
     //check to see if there is a next attribute
-    if(data.next){
+    if (data.next) {
         onward.setAttribute('onclick', `learnSW('${data.next}')`);
     }
     //check to see if there is a previous attribute
-    if(data.previous){
+    if (data.previous) {
         back.setAttribute('onclick', `learnSW('${data.previous}')`);
     }
+}
+
+function showDetails(id,height,mass) {
+    // console.log();
+    var li = document.getElementById(id);
+    var div = document.createElement('div' );
+    div.innerHTML =
+        `
+    <div id= 'details'>
+    <p>Height ${height}</p> <p>Mass ${mass}</p>
+    </div>
+    `
+    li.appendChild(div);
 }
 
