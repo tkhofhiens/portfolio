82cd8779214179ce04369add47c9fbf30b865860
tkhofhiens <tkhofhiens@gmail.com>
2020-11-19 15:41:32 -0700

    updates to game (score and time)

--- challenge/js/main.js
+++ challenge/js/main.js
@@ -11,19 +11,19 @@ document.querySelector('#mySelect').onchange = bookFilter;
 
 loadScriptures();
 
-function loadScriptures(){
+function loadScriptures() {
   const scriptureList = ls.getScriptureList();
   renderList(scriptureList);
 }
 
-function renderList(list){
+function renderList(list) {
   list.forEach(scripture => {
     const el = createScriptureElement(scripture)
     addToList(el);
-});
+  });
 }
 
-function createScriptureElement(scripture){
+function createScriptureElement(scripture) {
   // Scripture div
   const scriptureDiv = document.createElement('div');
   scriptureDiv.classList.add('scripture');
@@ -33,7 +33,7 @@ function createScriptureElement(scripture){
   scriptureReference.innerText = scripture.Reference;
   scriptureReference.classList.add('reference');
 
- 
+
   // scripture Phrase
   const scripturePhrase = document.createElement('div');
   scripturePhrase.innerText = scripture.Phrase;
@@ -47,25 +47,25 @@ function createScriptureElement(scripture){
 }
 
 
-function addToList(scriptureDiv){
+function addToList(scriptureDiv) {
   // add to the document 
   document.querySelector('#scriptureList').appendChild(scriptureDiv);
 }
 
-function bookFilter(){
-  document.querySelector('#scriptureList').innerHTML= '';
+function bookFilter() {
+  document.querySelector('#scriptureList').innerHTML = '';
   console.log('bookFilter');
   var book = document.getElementById("mySelect").value;
   document.getElementById("demo").innerHTML = "You selected: " + book;
   const allScriptures = ls.getScriptureList();
-  let filteredList = [];	
+  let filteredList = [];
 
   // allScriptures.forEach(scripture => {
   //   if (scripture.Book == book){
   // let filterList = 
   //   }
   // });
-  filteredList = allScriptures.filter(s =>{
+  filteredList = allScriptures.filter(s => {
     s.Book == book;
     console.log(s.Book, book);
   })
